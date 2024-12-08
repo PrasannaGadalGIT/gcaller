c8e584ef42e973eeef534e26e83339fa1f29c2b3
Prasanna Gadal <gadalprasanna5@gmail.com>
2024-12-08 12:24:45 +0000

    Profile page

---
+++ app/contacts/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contacts = () => {
+  return (
+    <div className='bg-black min-h-screen pt-10 px-4 text-white'>
+      <h1>Contacts</h1>
+    </div>
+  )
+}
+
+export default Contacts
+
+
