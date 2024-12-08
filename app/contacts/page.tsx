55856cf2229796646390b4583cccc086d71a80d4
Prasanna Gadal <gadalprasanna5@gmail.com>
2024-12-08 12:48:24 +0000

    responsve

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Contacts = () => {
   return (
     <div className='bg-black min-h-screen pt-10 px-4 text-white'>
-      <h1>Contacts</h1>
+      <div className=' flex justify-between'>
+        <h1>Contacts</h1>
+        <h1 className=' text-[#9747FF]'>+ Import Contacts</h1>
+      </div>
+      <div className="h-[40px] w-full bg-[#9747FF] opa rounded-lg mt-3 flex items-center px-3" style={{ backgroundColor: "rgba(151, 71, 255, 0.3)" }}>
+      <input
+        type="text"
+        placeholder='Search'
+        className="bg-transparent w-full h-full text-white placeholder-gray-200 outline-none"
+      />
+    </div>
+
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import CallBalance from "@/components/CallBalance";
 
 export default function Home() {
   return (
-    <div className="bg-black font-poppins w-full">
+    <div className="bg-black font-poppins w-full ">
       <Image
         src={"/assets/pp.png"}
         alt="Profile Picture"
@@ -17,44 +17,46 @@ export default function Home() {
       />
 
       <div className="bg-black relative -top-3 z-10 rounded-t-2xl h-screen w-full">
-        <div className="h-full bg-gradient-to-b from-purple-900 via-purple-950 to-black rounded-t-2xl opacity-60"></div>
-      </div>
+        <div className="h-full bg-gradient-to-b rounded-t-2xl flex flex-col items-center" style={{
+          background: "linear-gradient(to bottom, rgba(75, 0, 130, 0.6), rgba(48, 0, 87, 0.6), rgba(0, 0, 0, 0.6))"
+        }}>
+          <h1 className="text-white text-4xl md:text-5xl lg:text-6xl mt-5">Welcome Kash</h1>
 
-      <div className="absolute top-[294px] left-0 right-0 bottom-0 z-10 p-5 flex flex-col items-center w-full md:top-[40rem] lg:top-[75rem]">
-        <h1 className="text-white text-4xl md:text-5xl lg:text-6xl">Welcome Kash</h1>
-
-        {/* Pubkey */}
-        <div className="flex justify-center mt-3 w-full sm:w-[80%] md:w-[60%] lg:w-[50%]">
-          <p className="truncate w-[50%] text-gray-500 text-sm sm:text-base">{`BmQuXK4wJdLEULMvzwyiNE9p7Rj3Pg4pgFfoB1SY53pj`}</p>
-          <ClipboardComponent value="BmQuXK4wJdLEULMvzwyiNE9p7Rj3Pg4pgFfoB1SY53pj" />
-        </div>
-
-        {/* Border */}
-        <DashedBorder />
-
-        <div>
-          <CallBalance amount={20} />
-
-          <div className="flex justify-between mt-3 ">
-            <Card
-              imageurl="/assets/phone.png"
-              smallText="Total"
-              biggerText="Contacts"
-              numberOfSpamOrContacts={100}
-            />
-            <div className=" ml-1"></div>
-            <Card
-              imageurl="/assets/verified.png"
-              smallText="Spam"
-              biggerText="Detected"
-              numberOfSpamOrContacts={30}
-            />
+          {/* Pubkey */}
+          <div className="flex justify-center mt-3 w-full sm:w-[80%] md:w-[60%] lg:w-[50%]">
+            <p className="truncate w-[50%] text-gray-500 text-sm sm:text-base">{`BmQuXK4wJdLEULMvzwyiNE9p7Rj3Pg4pgFfoB1SY53pj`}</p>
+            <ClipboardComponent value="BmQuXK4wJdLEULMvzwyiNE9p7Rj3Pg4pgFfoB1SY53pj" />
           </div>
 
-       
+          {/* Border */}
+          <DashedBorder />
+
+          <div>
+            <CallBalance amount={20} />
+
+            <div className="flex justify-between mt-3 ">
+              <Card
+                imageurl="/assets/phone.png"
+                smallText="Total"
+                biggerText="Contacts"
+                numberOfSpamOrContacts={100}
+              />
+              <div className=" ml-1"></div>
+              <Card
+                imageurl="/assets/verified.png"
+                smallText="Spam"
+                biggerText="Detected"
+                numberOfSpamOrContacts={30}
+              />
+            </div>
+
+
+          </div>
+          <DashedBorder />
         </div>
-        <DashedBorder />
       </div>
+
+
     </div>
   );
 }
